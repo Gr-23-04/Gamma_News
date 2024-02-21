7862ebe7bc9c204ef886616071823d2c219a2004
Mauitron <Mauritz.nyfeldt@gmail.com>
2024-02-21 10:06:34 +0100

    Added a way to open and close the drop-down window (Thanks Wei!) Fixed for mobile aswell.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function() {
+    $("#dropdown_menu_button_desktop").click(function () {
+        $("#menu_drop_desktop").slideToggle("fast");
+    });
+    $("#dropdown_menu_button_mobile").click(function () {
+        $("#menu_drop_mobile").slideToggle("fast");
+    });
+    $("#close_menu_button_mobile").click(function () {
+        $("close_menu_mobile").slideToggle("fast");
+    });
+
+});
