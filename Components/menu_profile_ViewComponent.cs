a011312975942b5131ae6091b4b57bdf127e8db3
Mauitron <Mauritz.nyfeldt@gmail.com>
2024-02-25 14:17:25 +0100

    Did a whole lot of stuff.

--- Components/menu_profile_ViewComponent.cs
+++ Components/menu_profile_ViewComponent.cs
@@ -2,15 +2,16 @@
 
 namespace Gamma_News.Components
 {
+    [ViewComponent(Name = "profile")]
     public class menu_profile_ViewComponent : ViewComponent
     {
 
-        public async Task<IViewComponentResult> async_menu_viewcomp( )
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var some_function = 3;
 
 
-            return View( );
+            return View();
         }
 
     }
--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,25 +1,24 @@
 ﻿using Gamma_News.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace Gamma_News.Controllers
 {
-    public class WeatherController : Controller
-    {
-        private readonly WeatherService _weatherService;
-        public WeatherController(WeatherService weatherService) 
-        {
-            _weatherService = weatherService;
-        }
+	public class WeatherController : Controller
+	{
+		private readonly WeatherService _weatherService;
+		public WeatherController(WeatherService weatherService)
+		{
+			_weatherService = weatherService;
+		}
 
-        public async Task<IActionResult> Index(string city = "linköping") 
-        {
-            var weather = await _weatherService.GetWeatherAsync(city);
-            return View(weather);
-        }
-        public IActionResult details()
-        {
-            return View();
-        }
-    }
+		public async Task<IActionResult> Index(string city = "linköping")
+		{
+			var weather = await _weatherService.GetWeatherAsync(city);
+			return View(weather);
+		}
+		public IActionResult details()
+		{
+			return View();
+		}
+	}
 }
