88a7fd38fafbb4c31b9dfcbf19e6977fbf13d71d
Mauitron <Mauritz.nyfeldt@gmail.com>
2024-02-26 02:21:23 +0100

    Fixed menu, added ineractivity and somewhat centeterd the layout.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,13 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
 $(document).ready(function() {
+        //int menu = 1;
     $("#drop_menu_button_desktop").click(function () {
         $("#drop_menu_desktop").slideToggle("medium");
+        //$if (menu != 0) {
+        //    menu = 1
+        //$("#drop_profile_desktop").slideToggle("medium");
+        //}
     });
     $("#drop_profile_button_desktop").click(function () {
         $("#drop_profile_desktop").slideToggle("medium");
+        //$if (menu != 1) {
+        //    menu = 0;
+        //$("#drop_menu_desktop").slideToggle("medium");
+        //}
     });
     $("#menu_button_mobile").click(function () {
         $("#menu_mobile").slideToggle("medium");
