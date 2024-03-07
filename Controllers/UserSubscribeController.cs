6143de04b8fb1d3ae526322e4f210e633cf270af
MAJ <zhaoheyi6@gmail.com>
2024-03-07 09:21:17 +0100

    11 changes about Users subscribtion. Function probaböy can not be used but the layout of the subscribtion can show

---
+++ Controllers/UserSubscribeController.cs
@@ -0,0 +1,56 @@
+﻿using Gamma_News.Data;
+using Gamma_News.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Gamma_News.Controllers
+{
+    [Authorize] //user is authenticated
+    public class UserSubscribeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        //public SubscriptionsController(ApplicationDbContext context, UserManager<User> userManager)
+        //{ 
+        //    _context = context;
+        //    _userManager = userManager;
+
+        //}
+        public async Task<IActionResult> Indexshown()
+        { 
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+                return Challenge(); // Prompt user to log in
+
+            var subscriptions= await _context.Subscriptions
+                .Where(s=> s.UserId == currentUser.Id)
+                .Include(s => s.Article)  // Load the related articles
+                .Select(s => s.Article) // Select only the articles
+                .ToListAsync();
+            return View(subscriptions);// Pass the articles to the view
+
+
+
+
+
+        }
+        //Implement a create action in the SubscriptionsController
+        //that handles the logic when a user subscribes to an article.
+        //This action will create a new Subscription instance linking
+        //the user with the article.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create() //handles the logic when a user subscribes to an article. This action will
+                                      //create a new Subscription instance linking the user with the article.
+        {
+            return View();
+        }
+    }
+}
