06913a6d2a6ab093a48217d0d7fa38d1a37d3516
Heyi Zhao <zhaoheyi6@gmail.com>
2024-03-08 15:51:57 +0100

    8 changes in Homecontroller, usersubscribe controller, suscription.cs, user cs, layout.cshtml, menu.cshtml, login.cshtmlk

---
+++ Models/Subscription.cs
@@ -0,0 +1,40 @@
+﻿using Gamma_News.Data.Migrations;
+
+namespace Gamma_News.Models
+{
+    public class Subscription
+    {
+        //Subscription Information
+        //· Id
+        //· SubscriptionType
+        //· Price
+        //· Created
+        //· Expires
+        //· User
+        //· PaymentComplete
+        public int SubscriptionId { get; set; }
+        public string SubscriptionType { get; set; } = string.Empty;
+        public decimal SubscriptionPrice { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime PaymentComplete { get; set; }
+        public DateTime Expires { get; set; }
+
+        //SubscriptionType 
+        //· Id
+        //· TypeName
+        //· Description
+        //· Price
+
+        public int SubcriptionTypeId { get; set; }
+        public string SubcriptionTypeName { get; set; } = string.Empty;
+        public string SubscriptionTypeDescription { get; set; } = string.Empty;
+        public decimal SubscriptionTypePrice { get; set; }
+
+        //Navigation properties
+        public virtual Article Article { get; set; }
+        public virtual User User { get; set; }
+       
+    }
+}
