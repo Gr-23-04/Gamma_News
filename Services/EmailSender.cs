9041371e29a877ca20d23a4a7b1b817de897be2b
Mauitron <Mauritz.nyfeldt@gmail.com>
2024-04-13 16:28:32 +0200

    made the articles look a bit nicer

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,20 +1,22 @@
 ﻿using Gamma_News.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+
 
 namespace Gamma_News.Services;
 
 public class EmailSender : IEmailSender
 {
-    private readonly User _user;
+    private readonly UserManager<User> _user;
 
-    public EmailSender(User user)
+    public EmailSender(UserManager<User> user)
     {
         _user = user;
     }
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        email = _user.Email;
+
 
     }
 
