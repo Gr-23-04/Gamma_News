b74b5475e6f25d4977121d01c321c0a15b64878d
Mauitron <Mauritz.Nyfeldt@gmail.com>
2024-04-15 10:54:08 +0200

    changed the color for the text in the stock page. Added the About Page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -73,6 +73,10 @@ namespace Gamma_News.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
